074ca1d8ec8a52f0f41aa75f6da8bfbbb65451ea
Ibrahim Abuaker <ibrahim.aaker@gmail.com>
2024-04-02 14:45:13 +0200

    completed server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./dbinit");
+
+const PORT = process.env.PORT || 8090;
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.json("Welcome to my API");
+});
+
+app.listen(PORT, () => {
+  console.log("Listen on Port");
+});
